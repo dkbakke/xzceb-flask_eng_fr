8238ec6ce329807ae1a7640f5e9145837afe9d94
dkbakke <dbakkepublic@gmail.com>
2022-08-27 22:45:22 +0000

    Even more changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -7,10 +7,12 @@ class TestTranslator(unittest.TestCase):
     def test_englishToFrench(self):
         self.assertEqual(englishToFrench('Hello'),'Bonjour')
         self.assertNotEqual(englishToFrench('Goodbye'),'Bonjour')
+        self.assertRaises( ValueError,englishToFrench, None ) # test for null input
 
     def test_frenchToEnglish(self):
         self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
         self.assertNotEqual(frenchToEnglish('Bonjour'),'Goodbye')
+        self.assertRaises(ValueError,englishToFrench, None ) # test for null input
 
 if __name__=='__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,26 +1,24 @@
-import json
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson import ApiException
 import os
+from ibm_watson import LanguageTranslatorV3,ApiException
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
 load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
-version='2022-08-25'
+VERSION='2022-08-25'
 
 authenticator = IAMAuthenticator( apikey )
 language_translator = LanguageTranslatorV3(
-    version=version,
+    version=VERSION,
     authenticator=authenticator
 )
 
 language_translator.set_service_url(url)
 
 def englishToFrench(englishText):
-    '''Translate english text to french''' 
+    '''Translate english text to french'''
     try:
         translation = language_translator.translate(
             englishText,
@@ -28,19 +26,15 @@ def englishToFrench(englishText):
     except ApiException as ex:
         print ("Method failed with status code " + str(ex.code) + ": " + ex.message)
     frenchText = translation["translations"][0]["translation"]
-    print ( frenchText )
     return frenchText
-    
 
 def frenchToEnglish(frenchText):
-    '''Translate french text to english''' 
+    '''Translate french text to english'''
     try:
-        translation = language_translator.translate(
-            frenchText,
+        translation = language_translator.translate( frenchText,
             model_id='fr-en').get_result()
     except ApiException as ex:
         print ( "Method failed with status code " + str(ex.code) + ": " + ex.message)
     englishText = translation["translations"][0]["translation"]
-    print ( englishText )
     return englishText
     
